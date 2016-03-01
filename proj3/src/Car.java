4b818bedff909aadd4e52bc882028f086d88b165
Pchel666 <pchel66694@gmail.com>
2016-03-01 13:47:53 -0500

    added car class, deleted unused GUI class

--- proj3/src/Car.java
+++ proj3/src/Car.java
@@ -5,4 +5,119 @@ public class Car {
     private String name;
     private String engine;
     private String tire;
+
+    public Car(){
+        weight = 100;
+        color = "blue";
+        name = "car1";
+        engine = "medium";
+        tire = "regular";
+        maxSpeed = 50;
+        switch (engine.toLowerCase()){
+            case "small":
+                maxSpeed = maxSpeed + 10;
+                break;
+            case "medium":
+                maxSpeed = maxSpeed + 20;
+                break;
+            case "large":
+                maxSpeed = maxSpeed + 30;
+                break;
+            default:
+                break;
+        }
+        maxSpeed = maxSpeed / (weight / 50);
+    }
+
+    public Car(int weight, String color, String name, String engine, String tire){
+        this.weight = weight;
+        this.color = color;
+        this.name = name;
+        this.engine = engine;
+        this.tire = tire;
+        maxSpeed = 50;
+        switch (engine.toLowerCase()){
+            case "small":
+                maxSpeed = maxSpeed + 10;
+                break;
+            case "medium":
+                maxSpeed = maxSpeed + 20;
+                break;
+            case "large":
+                maxSpeed = maxSpeed + 30;
+                break;
+            default:
+                break;
+        }
+        maxSpeed = maxSpeed / (weight / 50);
+    }
+
+    public int getMaxSpeed(){
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int speed, boolean formulaOrNot){
+        if (formulaOrNot)
+        {
+            maxSpeed = 50;
+            switch (engine.toLowerCase()){
+                case "small":
+                    maxSpeed = maxSpeed + 10;
+                    break;
+                case "medium":
+                    maxSpeed = maxSpeed + 20;
+                    break;
+                case "large":
+                    maxSpeed = maxSpeed + 30;
+                    break;
+                default:
+                    break;
+            }
+            maxSpeed = maxSpeed / weight;
+        }
+        else
+        {
+            maxSpeed = speed;
+        }
+    }
+
+    public int getWeight(){
+        return weight;
+    }
+
+    public void setWeight(int weight){
+        this.weight = weight;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getEngine(){
+        return engine;
+    }
+
+    public void setEngine(String engine){
+        this.engine = engine;
+    }
+
+    public String getTire(){
+        return tire;
+    }
+
+    public void setTire(String tire){
+        this.tire = tire;
+    }
 }
