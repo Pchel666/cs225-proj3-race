3cfab52f05204f481377d36290d46db98bb4dacf
ITheGuy <s_gilani@post.massbay.edu>
2016-03-11 23:25:50 -0500

    Added files via upload

--- proj3/src/Location.java
+++ proj3/src/Location.java
@@ -1,19 +1,48 @@
+/**
+ * This class is used to create a Location of any object on a map.
+ * @author Syed Gilani, Cory Skinner, Alex Popov
+ * @version 3.11.16
+ */
 public class Location {
     private int xCoord;
     private int yCoord;
+    private String name;
 
-    public Location(int xCoord, int yCoord){
+    /**
+     * The constructor of the Location class.
+     * @param xCoord sets the x coord
+     * @param yCoord sets the y coord
+     * @param name sets the name on the map
+     */
+    public Location(int xCoord, int yCoord, String name){
         this.xCoord = xCoord;
         this.yCoord = yCoord;
+        this.name = name;
     }
 
+    /**
+     * Get the x coord of the object.
+     * @return an int, the x coord
+     */
     public int getXCoord(){
         return xCoord;
     }
 
+    /**
+     * Get the y coord of the object
+     * @return an int, the y coord
+     */
     public int getYCoord() {
         return yCoord;
     }
+
+    /**
+     * Get the name of the location
+     * @return a String, the name of the location
+     */
+    public String getName() { return name; }
+
+    public void setName(String newName) { name = newName; }
 
     public void setXCoord(int xCoord) {
         this.xCoord = xCoord;
--- proj3/src/Map.java
+++ proj3/src/Map.java
@@ -2,18 +2,26 @@ import java.util.ArrayList;
 import java.util.Random;
 
 /**
+ * This class is used to create a map object with locations and cities randomly placed on it.
+ * The locations on the map are assigned 3 types of terrain or a city, the key is below.
  * locations: int[][], one of four values:
  * 0 = Regular
  * 1 = Dirt
  * 2 = Snow
  * 3 = City
+ * @author Syed Gilani, Cory Skinner, Alex Popov
+ * @version 3.11.16
  */
 public class Map {
     private int[][] locations;
     private Random rand;
-    ArrayList<Location> cities;
+    private ArrayList<Location> cities;
     private static final int numOfTerrain = 3;
 
+    /**
+     * The Map constructor take a map size that is squared to create the map.
+     * @param size the size of the map
+     */
     public Map(int size) {
         rand = new Random();
         this.locations = new int[size][size];
@@ -22,36 +30,47 @@ public class Map {
                 locations[i][j] = rand.nextInt(numOfTerrain);
             }
         }
-        // For four quadrants
+        makeFourCities(size);
+    }
+
+    /**
+     * Get locations on the map
+     * @return an int[][], get location of cities
+     */
+    public int[][] getLocations() { return locations; }
+    public ArrayList<Location> getCities() {
+        return cities;
+    }
+
+    /**
+     * Add four cities to the Map
+     * @param size the size in the map
+     */
+    private void makeFourCities(int size) {
         int x;
         int y;
         int halfSize = size/2;
         cities = new ArrayList<Location>();
-
+        //Split into four quadrants
         // 5, 5
-        x = rand.nextInt(halfSize + 1);
-        y = rand.nextInt(halfSize + 1);
+        x = rand.nextInt(halfSize);
+        y = rand.nextInt(halfSize);
         locations[x][y] = 3;
-        cities.add(new Location(x, y));
+        cities.add(new Location(x, y, "CityA"));
         // 10, 5
-        x = rand.nextInt(halfSize + 1) + halfSize;
-        y = rand.nextInt(halfSize + 1);
+        x = rand.nextInt(halfSize) + halfSize;
+        y = rand.nextInt(halfSize);
         locations[x][y] = 3;
-        cities.add(new Location(x, y));
+        cities.add(new Location(x, y, "CityB"));
         // 5, 10
-        x = rand.nextInt(halfSize + 1);
-        y = rand.nextInt(halfSize + 1) + halfSize;
+        x = rand.nextInt(halfSize);
+        y = rand.nextInt(halfSize) + halfSize;
         locations[x][y] = 3;
-        cities.add(new Location(x, y));
+        cities.add(new Location(x, y, "CityC"));
         // 10, 10
-        x = rand.nextInt(halfSize + 1) + halfSize;
-        y = rand.nextInt(halfSize + 1) + halfSize;
+        x = rand.nextInt(halfSize) + halfSize;
+        y = rand.nextInt(halfSize) + halfSize;
         locations[x][y] = 3;
-        cities.add(new Location(x, y));
-    }
-
-    public int[][] getLocations() { return locations; }
-    public ArrayList<Location> getCities() {
-        return cities;
+        cities.add(new Location(x, y, "CityD"));
     }
 }
